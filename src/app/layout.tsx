b895c6976fc34f5e27dbed5fc1f6de7e4d56990c
Anton Siligan <anton.siligan@neinja.dev>
2025-09-27 11:06:00 +0200

    add theming and button component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import { ThemeProvider } from '@/components/theming/theme-provider';
 
 export const metadata: Metadata = {
 	title: 'EconSpector v2',
@@ -13,7 +14,11 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<body className='antialiased'>{children}</body>
+			<body className='antialiased'>
+				<ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
+					{children}
+				</ThemeProvider>
+			</body>
 		</html>
 	);
 }
