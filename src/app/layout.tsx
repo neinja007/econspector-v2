8228a66a4875af51f4f8432103942d8b176af3e1
Anton Siligan <anton.siligan@neinja.dev>
2025-09-28 10:42:07 +0200

    centre clerk components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ const RootLayout = ({ children }: RootLayoutProps) => {
 						<TanstackQueryProvider>
 							<SidebarProvider>
 								<AppSidebar />
-								<main className='p-4'>{children}</main>
+								<main className='p-4 w-full h-screen'>{children}</main>
 							</SidebarProvider>
 						</TanstackQueryProvider>
 					</ThemeProvider>
---
+++ src/app/user/layout.tsx
@@ -0,0 +1,7 @@
+type LayoutProps = { children: React.ReactNode };
+
+const Layout = ({ children }: LayoutProps) => {
+	return <div className='flex items-center justify-center h-screen w-full'>{children}</div>;
+};
+
+export default Layout;
