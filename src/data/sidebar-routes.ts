0209f37bfe6171abbbc62b4a4c00f43f8702b64d
Anton Siligan <anton.siligan@neinja.dev>
2025-09-29 16:36:02 +0200

    add missing sign in route

--- src/data/sidebar-routes.ts
+++ src/data/sidebar-routes.ts
@@ -14,6 +14,7 @@ import {
 	Home,
 	Landmark,
 	Lightbulb,
+	LogIn,
 	LucideIcon,
 	MapIcon,
 	MessagesSquare,
@@ -163,6 +164,11 @@ export const sidebarRoutes: {
 				label: 'Manage Account',
 				href: '/user/account',
 				icon: Settings
+			},
+			{
+				label: 'Sign In or Register',
+				href: '/user/sign-in',
+				icon: LogIn
 			}
 		]
 	},
