231bdae8b65da31922c2ee14017a46eddde5ab1e
Anton Siligan <anton.siligan@neinja.dev>
2025-10-03 15:46:20 +0200

    add indicator admin script

---
+++ src/app/api/(private)/indicators/route.ts
@@ -0,0 +1,63 @@
+import { indicators } from '../../../../../private/raw-data/indicators';
+import { NextResponse } from 'next/server';
+import { adminSupabase } from '@/supabase/admin';
+
+export const GET = async () => {
+	if (process.env.ENABLE_ADMIN_SCRIPTS !== 'true') {
+		return NextResponse.json({ message: 'Admin scripts are not enabled. Nice try.' }, { status: 403 });
+	}
+
+	const existingIndicators = await adminSupabase.schema('data').from('indicators').select('*');
+	const skippedIndicators = [];
+	const createdIndicators = [];
+
+	for (const indicator of indicators) {
+		if (existingIndicators.data?.some((i) => i.name === indicator.name)) {
+			skippedIndicators.push(indicator.name);
+			continue;
+		} else {
+			createdIndicators.push(indicator);
+		}
+
+		if ('subindicators' in indicator) {
+			const dbIndicator = await adminSupabase
+				.schema('data')
+				.from('indicators')
+				.insert({ name: indicator.name })
+				.select()
+				.single();
+
+			for (const subindicator of indicator.subindicators) {
+				const dbSubindicator = await adminSupabase
+					.schema('data')
+					.from('subindicators')
+					.insert({ name: subindicator.name, parent_id: dbIndicator.data.id })
+					.select()
+					.single();
+
+				for (const frequency of subindicator.frequencies) {
+					const dbFrequency = await adminSupabase
+						.schema('data')
+						.from('frequencies')
+						.insert({ frequency: frequency.frequency, indicator_id: dbSubindicator.data.id })
+						.select()
+						.single();
+
+					for (const source of frequency.sources) {
+						await adminSupabase
+							.schema('data')
+							.from('sources')
+							.insert({ frequency_id: dbFrequency.data.id, name: source.source, unit: source.unit })
+							.select()
+							.single();
+					}
+				}
+			}
+		}
+	}
+	return NextResponse.json({
+		message: 'Indicators added to database',
+		created: createdIndicators,
+		skipped: skippedIndicators
+	});
+};
