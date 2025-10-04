1c49c1d74b3f1e1798e094b5589d4de633121b9c
Anton Siligan <anton.siligan@neinja.dev>
2025-10-04 11:09:44 +0200

    modify script

--- private/scripts/compare_countries_with_wb.ts
+++ private/scripts/compare_countries_with_wb.ts
@@ -1,1 +1,13 @@
-console.log('Hello, world!');
+import { countries } from '../raw-data/general/countries/countries';
+import { world_bank_countries } from '../raw-data/world_bank/countries';
+import { world_bank_regions } from '../raw-data/world_bank/regions';
+
+const countriesWithoutRegions = world_bank_countries.filter(
+	(country) => !world_bank_regions.some((region) => region.code === country.id)
+);
+
+console.log(
+	countries
+		.filter((country) => !countriesWithoutRegions.some((wbCountry) => wbCountry.id === country.cca3))
+		.map((country) => country.cca3)
+);
