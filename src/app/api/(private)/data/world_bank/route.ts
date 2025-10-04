3c2310cd7dc27b1433db155a8707314b16a6cfe6
Anton Siligan <anton.siligan@neinja.dev>
2025-10-04 11:32:07 +0200

    implement world bank scripts

--- src/app/api/(private)/data/world_bank/route.ts
+++ src/app/api/(private)/data/world_bank/route.ts
@@ -1,9 +1,62 @@
-import { DatabaseTable } from '@/data/supabase';
-import { supabase } from '@/supabase/client';
+import { DatabaseSchema, DatabaseTable } from '@/data/supabase';
 import { NextResponse } from 'next/server';
+import axios from 'axios';
+import { adminSupabase } from '@/supabase/admin';
 
 export const POST = async () => {
-	const worldBankCountries = await supabase.from(DatabaseTable.COUNTRIES).select('*').eq('world_bank_data', true);
+	const worldBankCountries = await adminSupabase
+		.schema(DatabaseSchema.DATA)
+		.from(DatabaseTable.COUNTRIES)
+		.select('*')
+		.eq('world_bank', true);
 
-	return NextResponse.json(worldBankCountries);
+	if (!worldBankCountries.data) {
+		return NextResponse.json({ error: 'No world bank countries found' }, { status: 404 });
+	}
+
+	const worldBankSources = await adminSupabase
+		.schema(DatabaseSchema.DATA)
+		.from(DatabaseTable.FREQUENCY_SOURCES)
+		.select('*')
+		.eq('name', 'World Bank');
+
+	if (!worldBankSources.data) {
+		return NextResponse.json({ error: 'No world bank sources found' }, { status: 404 });
+	}
+
+	for (const country of worldBankCountries.data) {
+		for (const source of worldBankSources.data) {
+			const data = await axios
+				.get(
+					`https://api.worldbank.org/v2/countries/${country.country_code}/indicators/${source.code}?per_page=10000&format=json`
+				)
+				.then((res) => res.data[1]);
+
+			const result = await adminSupabase
+				.schema(DatabaseSchema.DATA)
+				.from(DatabaseTable.WORLD_BANK_DATA)
+				.insert(
+					data
+						.filter((item: { value: number; date: string }) => item.value !== null)
+						.map((item: { value: number; date: string }) => ({
+							country_code: country.country_code,
+							source_id: source.id,
+							value: item.value,
+							period: item.date
+						}))
+				);
+		}
+	}
+
+	return NextResponse.json({ message: 'World bank data imported successfully' });
+};
+
+export const DELETE = async () => {
+	if (process.env.ENABLE_ADMIN_SCRIPTS !== 'true') {
+		return NextResponse.json({ message: 'Admin scripts are not enabled. Nice try.' }, { status: 403 });
+	}
+
+	await adminSupabase.schema(DatabaseSchema.DATA).from(DatabaseTable.WORLD_BANK_DATA).delete().neq('id', 0);
+
+	return NextResponse.json({ message: 'World bank data deleted successfully' });
 };
