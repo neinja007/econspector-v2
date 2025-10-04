056e53295723302163dfbd6ffb0c1f62eb20144a
Anton Siligan <anton.siligan@neinja.dev>
2025-10-04 17:30:20 +0200

    improve padding

--- src/components/sidebar/sidebar-header.tsx
+++ src/components/sidebar/sidebar-header.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export const SidebarHeader = () => {
 	return (
 		<SidebarHeaderShadcn className='w-full border-b'>
-			<div className='flex w-full gap-2 items-center justify-center p-1'>
+			<div className='flex w-full gap-2 items-center justify-center py-1'>
 				<Image src='/favicon.ico' alt='EconSpector' width={40} height={40} />{' '}
 				<div className='flex flex-col'>
 					<div className='flex items-baseline justify-between gap-1'>
