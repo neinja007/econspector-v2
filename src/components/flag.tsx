1d4e3ec72367aa7efc79ffb1026628025de4f992
Anton Siligan <anton.siligan@neinja.dev>
2025-10-04 18:41:28 +0200

    rework flag component

--- src/components/flag.tsx
+++ src/components/flag.tsx
@@ -1,7 +1,15 @@
 import Image from 'next/image';
 
-const Flag = ({ code, ratio }: { code: string; ratio: '1x1' | '4x3' }) => {
-	return <Image src={`flags/${ratio}/${code}.svg`} alt='flag' width={50} height={9} />;
+type FlagProps = {
+	code: string;
+	ratio: '1x1' | '4x3';
+	height: number;
+};
+
+const Flag = ({ code, ratio, height }: FlagProps) => {
+	const width = ratio === '1x1' ? height : (height * 4) / 3;
+
+	return <Image src={`flags/${ratio}/${code}.svg`} alt='flag' width={width} height={height} />;
 };
 
 export default Flag;
