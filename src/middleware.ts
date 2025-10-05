819d2f0008c0cd136c2b1a35ebefa78dd9c6b812
Anton Siligan <anton.siligan@neinja.dev>
2025-10-05 09:40:07 +0200

    protect admin routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,25 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
-export default clerkMiddleware();
+export default clerkMiddleware(async (auth, request) => {
+	if (process.env.ENABLE_ADMIN_SCRIPTS !== 'true') {
+		const { pathname } = request.nextUrl;
+		if (pathname.startsWith('/api/admin')) {
+			return NextResponse.json(
+				{
+					message:
+						'Admin scripts are not enabled. Please stop trying to access this stuff to ruin my platform, I am simply trying to do good in this world.'
+				},
+				{
+					status: 403
+				}
+			);
+		}
+
+		return NextResponse.next();
+	}
+});
+
+export const config = {
+	matcher: ['/api/(.*)']
+};
