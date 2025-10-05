f189b50e29f3d6231d22dd381fae60f688c751af
Anton Siligan <anton.siligan@neinja.dev>
2025-10-05 09:19:38 +0200

    fix image src

--- src/components/flag.tsx
+++ src/components/flag.tsx
@@ -9,7 +9,15 @@ type FlagProps = {
 const Flag = ({ code, ratio, height }: FlagProps) => {
 	const width = ratio === '1x1' ? height : (height * 4) / 3;
 
-	return <Image src={`/flags/${ratio}/${code}.svg`} alt='flag' className='rounded-md' width={width} height={height} />;
+	return (
+		<Image
+			src={`/flags/${ratio}/${code.toLowerCase()}.svg`}
+			alt='flag'
+			className='rounded-md'
+			width={width}
+			height={height}
+		/>
+	);
 };
 
 export default Flag;
