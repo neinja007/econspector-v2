e22a9b72284bdc76b0f475021c47ce2c854ba23d
Anton Siligan <anton.siligan@neinja.dev>
2025-10-05 10:54:38 +0200

    fix flag replacement display for regions

--- src/app/countries-and-regions/analysis/components/search-results.tsx
+++ src/app/countries-and-regions/analysis/components/search-results.tsx
@@ -14,14 +14,16 @@ export const SearchResults = ({ searchResults }: SearchResultsProps) => {
 				<Link
 					href={`/countries-and-regions/analysis/${slug(element.data.name)}`}
 					className='border rounded-md p-4 flex gap-4 justify-between hover:bg-accent hover:border-accent-foreground transition-colors'
-					key={slug(element.data.name)}
+					key={slug(element.type === 'country' ? element.data.country_code : element.data.code)}
 				>
 					{element.type === 'country' ? (
 						<div className='shrink-0'>
-							<Flag code={element.data.cca2} ratio='4x3' height={70} />
+							<Flag code={element.data.cca2} ratio='4x3' height={72} />
 						</div>
 					) : (
-						<div className='w-10 h-10 shrink-0'>{element.data.code}</div>
+						<div className='w-24 h-18 bg-gray-500 rounded-md flex items-center justify-center shrink-0'>
+							{element.data.code}
+						</div>
 					)}
 					<div className='flex flex-col items-end justify-between'>
 						<span className='line-clamp-2 text-right'>{element.data.name}</span>
