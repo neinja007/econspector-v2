eca62ca22a656fc3f7d3d8ad3afa4b1815679e0f
Anton Siligan <anton.siligan@neinja.dev>
2025-10-07 17:36:42 +0200

    fix countries currencies bridge

--- private/scripts/get_countries_currencies_csv.js
+++ private/scripts/get_countries_currencies_csv.js
@@ -1,30 +1,45 @@
 import fs from 'fs';
 
-const raw = fs.readFileSync('private/raw-data/countries.json', 'utf-8');
-const rawCurrencies = fs.readFileSync('private/raw-data/currencies.json', 'utf-8');
+const raw = fs.readFileSync('private/raw-data/general/countries/countries.json', 'utf-8');
+const rawDbCountries = fs.readFileSync('private/raw-data/general/countries/db/countries.json', 'utf-8');
+const rawCurrencies = fs.readFileSync('private/raw-data/general/currencies/currencies.json', 'utf-8');
+const rawDbCurrencies = fs.readFileSync('private/raw-data/general/currencies/db/currencies.json', 'utf-8');
 const countries = JSON.parse(raw);
+const dbCountries = JSON.parse(rawDbCountries);
 const currencies = JSON.parse(rawCurrencies);
+const dbCurrencies = JSON.parse(rawDbCurrencies);
 
 const rows = [];
 
-for (const country of countries) {
-	const countryCode = country.cca3;
-	if (country.currencies) {
-		for (const currencyCode of Object.keys(country.currencies)) {
-			if (currencies[currencyCode]) {
-				rows.push({
-					country_code: countryCode,
-					currency_code: currencyCode
-				});
-			} else {
-				console.log(`Currency ${currencyCode} not found in currencies.json`);
-			}
+/**
+ * To run this file in the terminal, use:
+ *
+ *   node private/scripts/get_countries_currencies_csv.js
+ *
+ * Make sure you are in the project root directory and have Node.js installed.
+ */
+
+for (const country of dbCountries) {
+	const nonDbCountryData = countries.find((co) => co.cca3 === country.country_code);
+	if (!nonDbCountryData) {
+		console.log(`Country ${country.country_code} not found in non-db countries.json`);
+		continue;
+	}
+	const countryCurrencyData = nonDbCountryData.currencies;
+	for (const currencyCode of Object.keys(countryCurrencyData)) {
+		if (dbCurrencies.find((c) => c.currency_code === currencyCode)) {
+			rows.push({
+				country_code: country.country_code,
+				currency_code: currencyCode
+			});
+		} else {
+			console.log(`Currency ${currencyCode} not found in db currencies.json`);
 		}
 	}
 }
 
 const csv = 'country_code,currency_code\n' + rows.map((row) => `${row.country_code},${row.currency_code}`).join('\n');
 
-fs.writeFileSync('private/scripts/output/country_currencies.csv', csv);
+fs.writeFileSync('private/scripts/output/countries_currencies.csv', csv);
 
-console.log('✅ country_currencies.csv generated successfully!');
+console.log('✅ countries_currencies.csv generated successfully!');
