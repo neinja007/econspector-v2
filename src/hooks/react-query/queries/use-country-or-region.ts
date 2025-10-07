ee8cbd9a48490c65ba91cee4a2a5755fb6a44bb8
Anton Siligan <anton.siligan@neinja.dev>
2025-10-07 17:55:03 +0200

    fix type

--- src/hooks/react-query/queries/use-country-or-region.ts
+++ src/hooks/react-query/queries/use-country-or-region.ts
@@ -1,13 +1,13 @@
 import { getCountry } from '@/api/country';
 import { getRegion } from '@/api/region';
 import { getSubregion } from '@/api/subregion';
-import { Country } from '@/types/country';
+import { CountryWithCurrencies } from '@/types/country';
 import { Region } from '@/types/region';
 import { Subregion } from '@/types/subregion';
 import { useQuery } from '@tanstack/react-query';
 
 export function useCountryOrRegion(type: 'country' | 'region' | 'subregion', code: string) {
-	return useQuery<Country | Region | Subregion>({
+	return useQuery<CountryWithCurrencies | Region | Subregion>({
 		queryKey: ['country-or-region', type, code],
 		queryFn: () => (type === 'country' ? getCountry(code) : type === 'region' ? getRegion(code) : getSubregion(code))
 	});
