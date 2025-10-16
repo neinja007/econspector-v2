1927b132c9f130b11dd6f0e0fd7141ede1062fbc
Anton Siligan <anton.siligan@neinja.dev>
2025-10-16 17:32:22 +0200

    fix orientation

--- src/app/countries-and-regions/analysis/[type]/[code]/components/currencies.tsx
+++ src/app/countries-and-regions/analysis/[type]/[code]/components/currencies.tsx
@@ -6,17 +6,19 @@ export const Currencies = ({ data }: { data: Currency[] }) => {
 	return (
 		<div className='flex flex-col gap-1 items-baseline text-muted-foreground'>
 			<span className='text-sm'>{data.length > 1 ? 'Currencies' : 'Currency'}:</span>
-			{data.map((currency) => (
-				<Button key={currency.currency_code} variant='outline' size='sm' asChild>
-					<Link
-						key={currency.currency_code}
-						className='flex items-center gap-1 transition-colors hover:text-blue-500'
-						href={`/currencies/analysis/${currency.currency_code}`}
-					>
-						{currency.symbol_native} {currency.name}
-					</Link>
-				</Button>
-			))}
+			<div className='flex gap-2'>
+				{data.map((currency) => (
+					<Button key={currency.currency_code} variant='outline' size='sm' asChild>
+						<Link
+							key={currency.currency_code}
+							className='flex items-center gap-1 transition-colors hover:text-blue-500'
+							href={`/currencies/analysis/${currency.currency_code}`}
+						>
+							{currency.symbol_native} {currency.name}
+						</Link>
+					</Button>
+				))}
+			</div>
 		</div>
 	);
 };
