467522703e66b16a0b8ec1dfebfa6a70c68838dd
Anton Siligan <anton.siligan@neinja.dev>
2025-10-16 17:05:30 +0200

    improve currency display

--- src/app/countries-and-regions/analysis/[type]/[code]/components/currencies.tsx
+++ src/app/countries-and-regions/analysis/[type]/[code]/components/currencies.tsx
@@ -1,5 +1,23 @@
+import { Badge } from '@/components/shadcn/ui/badge';
+import { Button } from '@/components/shadcn/ui/button';
 import { Currency } from '@/types/currency';
+import Link from 'next/link';
 
 export const Currencies = ({ data }: { data: Currency[] }) => {
-	return <div>{data.map((currency) => currency.name + ' (' + currency.symbol_native + ')').join(', ')}</div>;
+	return (
+		<div className='flex flex-col gap-1 items-baseline text-muted-foreground'>
+			<span className='text-sm'>{data.length > 1 ? 'Currencies' : 'Currency'}:</span>
+			{data.map((currency) => (
+				<Button key={currency.currency_code} variant='outline' size='sm' asChild>
+					<Link
+						key={currency.currency_code}
+						className='flex items-center gap-1 transition-colors hover:text-blue-500'
+						href={`/currencies/analysis/${currency.currency_code}`}
+					>
+						{currency.name} <Badge variant='outline'>{currency.symbol_native}</Badge>
+					</Link>
+				</Button>
+			))}
+		</div>
+	);
 };
