90a5382a3418fc6cddab11f49892fcc00be5f3a2
Anton Siligan <anton.siligan@neinja.dev>
2025-10-18 11:36:02 +0200

    extend indicator type

--- src/types/indicator.ts
+++ src/types/indicator.ts
@@ -1,7 +1,29 @@
+export type IndicatorCategory = {
+	id: string;
+	name: string;
+};
+
 export type Indicator = {
 	id: number;
 	parent_id: number;
 	name: string;
 	data_updated_at: string;
 	category_id: string;
+	indicator_frequencies: IndicatorFrequency[];
+};
+
+export type Frequency = 'Annual' | 'Biannual' | 'Monthly' | 'Quarterly';
+
+export type IndicatorFrequency = {
+	id: number;
+	indicator_id: number;
+	frequency: Frequency;
+	frequency_sources: FrequencySource[];
+};
+
+export type FrequencySource = {
+	id: number;
+	frequency_id: number;
+	source: string;
+	unit: string;
 };
