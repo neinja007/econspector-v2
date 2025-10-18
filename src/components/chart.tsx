c00488c85a57792c99ae8a801a76c6214148098c
Anton Siligan <anton.siligan@neinja.dev>
2025-10-18 12:50:13 +0200

    remove charts to component mapper

--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -1,11 +1,16 @@
 import { ChartType } from '@/types/chart';
 import { ChartConfig, ChartContainer, ChartLegend, ChartTooltip, ChartTooltipContent } from './shadcn/ui/chart';
-import { XAxis, YAxis } from 'recharts';
-import { chartComponents } from '@/data/charts';
+import {
+	XAxis,
+	YAxis,
+	AreaChart as RechartsAreaChart,
+	BarChart as RechartsBarChart,
+	LineChart as RechartsLineChart
+} from 'recharts';
 
 type ChartProps = {
 	data: { period: string; values: Record<string, number> }[];
-	type: ChartType;
+	type: ChartType | null;
 	unit: string;
 	config: ChartConfig;
 };
@@ -16,16 +21,17 @@ export const Chart = ({ data, type, unit, config }: ChartProps) => {
 		...item.values
 	}));
 
-	const ChartComponent = chartComponents[type];
+	const RechartsComponent =
+		type === ChartType.LINE ? RechartsLineChart : type === ChartType.BAR ? RechartsBarChart : RechartsAreaChart;
 
 	return (
 		<ChartContainer config={config} className='h-[200px] w-full'>
-			<ChartComponent data={chartData}>
+			<RechartsComponent data={chartData} accessibilityLayer>
 				<XAxis dataKey='period' />
 				<YAxis dataKey={unit} />
 				<ChartTooltip content={<ChartTooltipContent />} />
 				<ChartLegend />
-			</ChartComponent>
+			</RechartsComponent>
 		</ChartContainer>
 	);
 };
--- src/data/charts.ts
+++
@@ -1,8 +0,0 @@
-import { ChartType } from '@/types/chart';
-import { AreaChart, BarChart, LineChart } from 'recharts';
-
-export const chartComponents = {
-	[ChartType.AREA]: AreaChart,
-	[ChartType.LINE]: LineChart,
-	[ChartType.BAR]: BarChart
-};
