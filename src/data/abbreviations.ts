0a8f00960404df9093b1b4d1ea0c91f92dc1cea0
Anton Siligan <anton.siligan@neinja.dev>
2025-11-16 12:28:37 +0100

    add abbreviations file

---
+++ src/data/abbreviations.ts
@@ -0,0 +1,12 @@
+export const abbreviations: Record<
+	string,
+	{
+		short: string;
+		long: string;
+	}
+> = {
+	gdp: {
+		short: 'Gross Domestic Product. Often used as a measure of economic output.',
+		long: "Gross Domestic Product. Represents the total value of all goods and services produced within a country's borders."
+	}
+};
