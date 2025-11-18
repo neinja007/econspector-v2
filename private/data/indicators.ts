2cdb4110fc961597a4d24ae58fb106bedb902a2b
Anton Siligan <anton.siligan@neinja.dev>
2025-11-18 17:05:40 +0100

    add more indicators

--- private/data/indicators.ts
+++ private/data/indicators.ts
@@ -111,5 +111,180 @@ export const indicators: Indicator[] = [
 				]
 			}
 		]
+	},
+	{
+		name: 'GDP per capita',
+		subindicators: [
+			{
+				name: '% growth',
+				unit: '%',
+				chart_type: ChartType.BAR,
+				frequencies: [
+					{
+						frequency: Frequency.ANNUAL,
+						sources: [
+							{
+								source: DataSource.WORLD_BANK,
+								code: 'NY.GDP.PCAP.KD.ZG'
+							}
+						]
+					}
+				]
+			},
+			{
+				name: 'current US$',
+				unit: 'USD',
+				chart_type: ChartType.AREA,
+				frequencies: [
+					{
+						frequency: Frequency.ANNUAL,
+						sources: [
+							{
+								source: DataSource.WORLD_BANK,
+								code: 'NY.GDP.PCAP.CD'
+							}
+						]
+					}
+				]
+			},
+			{
+				name: 'PPP (current international US$)',
+				unit: 'USD',
+				chart_type: ChartType.AREA,
+				frequencies: [
+					{
+						frequency: Frequency.ANNUAL,
+						sources: [
+							{
+								source: DataSource.WORLD_BANK,
+								code: 'NY.GDP.PCAP.PP.CD'
+							}
+						]
+					}
+				]
+			}
+		]
+	},
+	{
+		name: 'Population',
+		subindicators: [
+			{
+				name: 'Total',
+				unit: 'people',
+				chart_type: ChartType.AREA,
+				frequencies: [
+					{
+						frequency: Frequency.ANNUAL,
+						sources: [
+							{
+								source: DataSource.WORLD_BANK,
+								code: 'SP.POP.TOTL'
+							}
+						]
+					}
+				]
+			},
+			{
+				name: 'Urban',
+				unit: 'people',
+				chart_type: ChartType.AREA,
+				frequencies: [
+					{
+						frequency: Frequency.ANNUAL,
+						sources: [
+							{
+								source: DataSource.WORLD_BANK,
+								code: 'SP.URB.TOTL'
+							}
+						]
+					}
+				]
+			},
+			{
+				name: 'Rural',
+				unit: 'people',
+				chart_type: ChartType.AREA,
+				frequencies: [
+					{
+						frequency: Frequency.ANNUAL,
+						sources: [
+							{
+								source: DataSource.WORLD_BANK,
+								code: 'SP.RUR.TOTL'
+							}
+						]
+					}
+				]
+			},
+			{
+				name: '% growth',
+				unit: '%',
+				chart_type: ChartType.BAR,
+				frequencies: [
+					{
+						frequency: Frequency.ANNUAL,
+						sources: [
+							{
+								source: DataSource.WORLD_BANK,
+								code: 'SP.POP.GROW'
+							}
+						]
+					}
+				]
+			}
+		]
+	},
+	{
+		name: 'Inflation',
+		subindicators: [
+			{
+				name: 'Consumer prices',
+				unit: '%',
+				chart_type: ChartType.BAR,
+				frequencies: [
+					{
+						frequency: Frequency.ANNUAL,
+						sources: [
+							{
+								source: DataSource.WORLD_BANK,
+								code: 'FP.CPI.TOTL.ZG'
+							}
+						]
+					}
+				]
+			},
+			{
+				name: 'GDP deflator',
+				unit: '%',
+				chart_type: ChartType.BAR,
+				frequencies: [
+					{
+						frequency: Frequency.ANNUAL,
+						sources: [
+							{
+								source: DataSource.WORLD_BANK,
+								code: 'NY.GDP.DEFL.KD.ZG'
+							}
+						]
+					}
+				]
+			}
+		]
+	},
+	{
+		name: 'Surface area',
+		unit: 'km²',
+		chart_type: ChartType.AREA,
+		frequencies: [
+			{
+				frequency: Frequency.ANNUAL,
+				sources: [
+					{
+						source: DataSource.WORLD_BANK,
+						code: 'AG.SRF.TOTL.K2'
+					}
+				]
+			}
+		]
 	}
 ];
