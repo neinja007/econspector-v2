77404f8210a2b8e4e7f1d66a90f91bde3fdd03d5
Anton Siligan <anton.siligan@neinja.dev>
2025-11-18 18:43:58 +0100

    use new icons

--- src/components/sidebar/sidebar-header.tsx
+++ src/components/sidebar/sidebar-header.tsx
@@ -5,8 +5,24 @@ import Link from 'next/link';
 export const SidebarHeader = () => {
 	return (
 		<SidebarHeaderShadcn className='w-full border-b'>
-			<div className='flex w-full gap-2 items-center justify-center py-1'>
-				<Image src='/favicon.ico' alt='EconSpector' width={40} height={40} />{' '}
+			<div className='flex w-full gap-4 items-center justify-center py-1'>
+				<div className='size-10 relative'>
+					<Image
+						src='/icons/water3.svg'
+						alt='EconSpector Water'
+						width={40}
+						height={40}
+						className='absolute top-0 left-0'
+					/>
+					<Image
+						src='/icons/coloured_land/green.svg'
+						alt='EconSpector Land'
+						width={40}
+						height={40}
+						className='absolute top-0 left-0 hue-rotate-30 shadow-md shadow-green-500 rounded-full'
+					/>
+				</div>
+				{/* <Image src='/favicon.ico' alt='EconSpector' width={40} height={40} />{' '} */}
 				<div className='flex flex-col'>
 					<div className='flex items-baseline justify-between gap-1'>
 						<span className='text-2xl font-medium'>EconSpector</span>
