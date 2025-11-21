b8f884303eaf85bae133604f0734b616c6be94b7
Anton Siligan <anton.siligan@neinja.dev>
2025-11-21 16:30:15 +0100

    remove jwt stuff

--- src/supabase/client.ts
+++ src/supabase/client.ts
@@ -1,27 +1,6 @@
-'use client';
-
-import { auth } from '@clerk/nextjs/server';
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
 	process.env.NEXT_PUBLIC_SUPABASE_URL!,
-	process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
-	{
-		global: {
-			fetch: async (url, options) => {
-				const authData = await auth();
-				const clerkToken = await authData.getToken({
-					template: 'Supabase'
-				});
-
-				const headers = new Headers(options?.headers);
-				headers.set('Authorization', `Bearer ${clerkToken}`);
-
-				return fetch(url, {
-					...options,
-					headers
-				});
-			}
-		}
-	}
+	process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
 );
