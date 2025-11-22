a0f79f116d81160920ddb19a29e42806b4b7a891
Anton Siligan <anton.siligan@neinja.dev>
2025-11-22 08:27:44 +0100

    use supabase auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,28 +1,20 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
-import { NextResponse } from 'next/server';
+import { updateSession } from '@/utils/shadcn/middleware'
+import { type NextRequest } from 'next/server'
 
-export default clerkMiddleware(async (auth, request) => {
-	if (process.env.ENABLE_ADMIN_SCRIPTS !== 'true') {
-		const { pathname } = request.nextUrl;
-		if (pathname.startsWith('/api/admin')) {
-			return NextResponse.json(
-				{
-					message:
-						'Admin scripts are not enabled. Please stop trying to access this stuff to ruin my platform, I am simply trying to do good in this world.'
-				},
-				{
-					status: 403
-				}
-			);
-		}
-
-		return NextResponse.next();
-	}
-});
+export async function middleware(request: NextRequest) {
+  return await updateSession(request)
+}
 
 export const config = {
-	matcher: [
-		'/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-		'/(api|trpc)(.*)'
-	]
-};
+  matcher: [
+    /*
+     * Match all request paths except:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
+     * Feel free to modify this pattern to include more paths.
+     */
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
+}
