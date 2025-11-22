a00fcf276c9d69009460efe990cb8368ecebcf52
Anton Siligan <anton.siligan@neinja.dev>
2025-11-22 10:44:04 +0100

    modify error page

--- src/app/auth/error/page.tsx
+++ src/app/auth/error/page.tsx
@@ -1,20 +1,10 @@
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/shadcn/ui/card';
-
 export default async function Page({ searchParams }: { searchParams: Promise<{ error: string }> }) {
 	const params = await searchParams;
 
 	return (
-		<Card>
-			<CardHeader>
-				<CardTitle className='text-2xl'>Sorry, something went wrong.</CardTitle>
-			</CardHeader>
-			<CardContent>
-				{params?.error ? (
-					<p className='text-sm text-muted-foreground'>Code error: {params.error}</p>
-				) : (
-					<p className='text-sm text-muted-foreground'>An unspecified error occurred.</p>
-				)}
-			</CardContent>
-		</Card>
+		<div className='text-xl'>
+			Sorry, something went wrong.{' '}
+			{params?.error ? <span>Error code: {params.error}</span> : <span>An unspecified error occurred.</span>}
+		</div>
 	);
 }
