6fd60109d51ba9a4b2862fd8df94b3291e7a26b0
Anton Siligan <anton.siligan@neinja.dev>
2025-11-26 09:49:17 +0100

    add more content to layout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,7 +1,25 @@
+import EconSpectorIcon from '@/components/icon';
+import Link from 'next/link';
+
 type LayoutProps = { children: React.ReactNode };
 
 const Layout = ({ children }: LayoutProps) => {
-	return <div className='flex mt-32 items-center justify-center p-6 md:p-10 w-full'>{children}</div>;
+	return (
+		<div className='flex mt-20 text-lg flex-col justify-center w-full max-w-xl mx-auto p-6 md:p-10 gap-6'>
+			<div className='w-full flex items-center justify-between'>
+				<div className='text-2xl font-bold'>EconSpector v2 Authentication</div>
+				<EconSpectorIcon />
+			</div>
+			<div className='w-full flex items-center justify-center'>{children}</div>
+			<div className='text-sm text-muted-foreground text-center'>
+				If you need help, please get in touch on our{' '}
+				<Link href='/support/help' className='text-blue-500 underline'>
+					Help & Contact
+				</Link>{' '}
+				page.
+			</div>
+		</div>
+	);
 };
 
 export default Layout;
