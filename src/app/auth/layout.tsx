2ff2302d714b50fb00d45813c951a8cfaba5ab20
Anton Siligan <anton.siligan@neinja.dev>
2025-11-26 10:14:05 +0100

    use proper semantic elements

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -6,18 +6,18 @@ type LayoutProps = { children: React.ReactNode };
 const Layout = ({ children }: LayoutProps) => {
 	return (
 		<div className='flex mt-20 text-lg flex-col justify-center w-full max-w-xl mx-auto p-6 md:p-10 gap-6'>
-			<div className='w-full flex items-center justify-between'>
+			<h2 className='w-full flex items-center justify-between'>
 				<div className='text-2xl font-bold'>EconSpector v2 Authentication</div>
 				<EconSpectorIcon />
-			</div>
-			<div className='w-full flex items-center justify-center'>{children}</div>
-			<div className='text-sm text-muted-foreground text-center'>
+			</h2>
+			<section className='w-full flex items-center justify-center'>{children}</section>
+			<footer className='text-sm text-muted-foreground text-center'>
 				If you need help, please get in touch on our{' '}
 				<Link href='/support/help' className='text-blue-500 underline'>
 					Support &gt; Help & Contact
 				</Link>{' '}
 				page.
-			</div>
+			</footer>
 		</div>
 	);
 };
