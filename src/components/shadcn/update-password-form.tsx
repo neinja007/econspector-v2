5855ace33a81dc6827dae98685b0027df2859a56
Anton Siligan <anton.siligan@neinja.dev>
2025-11-26 10:11:09 +0100

    add success state to update password

--- src/components/shadcn/update-password-form.tsx
+++ src/components/shadcn/update-password-form.tsx
@@ -5,15 +5,16 @@ import { Button } from '@/components/shadcn/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/shadcn/ui/card';
 import { Input } from '@/components/shadcn/ui/input';
 import { Label } from '@/components/shadcn/ui/label';
-import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { supabase } from '@/supabase/clients/client';
+import { Alert, AlertDescription, AlertTitle } from './ui/alert';
+import { CheckCircle } from 'lucide-react';
 
 export function UpdatePasswordForm({ className, ...props }: React.ComponentPropsWithoutRef<'div'>) {
 	const [password, setPassword] = useState('');
 	const [error, setError] = useState<string | null>(null);
 	const [isLoading, setIsLoading] = useState(false);
-	const router = useRouter();
+	const [success, setSuccess] = useState(true);
 
 	const handleForgotPassword = async (e: React.FormEvent) => {
 		e.preventDefault();
@@ -23,7 +24,7 @@ export function UpdatePasswordForm({ className, ...props }: React.ComponentProps
 		try {
 			const { error } = await supabase.auth.updateUser({ password });
 			if (error) throw error;
-			router.push('/');
+			setSuccess(true);
 		} catch (error: unknown) {
 			setError(error instanceof Error ? error.message : 'An error occurred');
 		} finally {
@@ -33,33 +34,41 @@ export function UpdatePasswordForm({ className, ...props }: React.ComponentProps
 
 	return (
 		<div className={cn('flex flex-col gap-6 w-full', className)} {...props}>
-			<Card>
-				<CardHeader>
-					<CardTitle className='text-2xl'>Reset Your Password</CardTitle>
-					<CardDescription>Please enter your new password below.</CardDescription>
-				</CardHeader>
-				<CardContent>
-					<form onSubmit={handleForgotPassword}>
-						<div className='flex flex-col gap-6'>
-							<div className='grid gap-2'>
-								<Label htmlFor='password'>New password</Label>
-								<Input
-									id='password'
-									type='password'
-									placeholder='New password'
-									required
-									value={password}
-									onChange={(e) => setPassword(e.target.value)}
-								/>
+			{success ? (
+				<Alert variant='success'>
+					<CheckCircle className='h-4 w-4' />
+					<AlertTitle className='text-xl'>Password Updated</AlertTitle>
+					<AlertDescription>Your password has been updated successfully.</AlertDescription>
+				</Alert>
+			) : (
+				<Card>
+					<CardHeader>
+						<CardTitle className='text-2xl'>Reset Your Password</CardTitle>
+						<CardDescription>Please enter your new password below.</CardDescription>
+					</CardHeader>
+					<CardContent>
+						<form onSubmit={handleForgotPassword}>
+							<div className='flex flex-col gap-6'>
+								<div className='grid gap-2'>
+									<Label htmlFor='password'>New password</Label>
+									<Input
+										id='password'
+										type='password'
+										placeholder='New password'
+										required
+										value={password}
+										onChange={(e) => setPassword(e.target.value)}
+									/>
+								</div>
+								{error && <p className='text-sm text-red-500'>{error}</p>}
+								<Button type='submit' className='w-full' disabled={isLoading}>
+									{isLoading ? 'Saving...' : 'Save new password'}
+								</Button>
 							</div>
-							{error && <p className='text-sm text-red-500'>{error}</p>}
-							<Button type='submit' className='w-full' disabled={isLoading}>
-								{isLoading ? 'Saving...' : 'Save new password'}
-							</Button>
-						</div>
-					</form>
-				</CardContent>
-			</Card>
+						</form>
+					</CardContent>
+				</Card>
+			)}
 		</div>
 	);
 }
