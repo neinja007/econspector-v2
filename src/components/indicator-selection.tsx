a22be613717eecb7efef3876e1bac042ffef002f
Anton Siligan <anton.siligan@neinja.dev>
2025-12-13 16:18:29 +0100

    add indicator selection combobox

--- src/components/indicator-selection.tsx
+++ src/components/indicator-selection.tsx
@@ -1,5 +1,21 @@
-export const IndicatorSelection = () => {
+import { useIndicators } from '@/hooks/react-query/queries/use-indicators';
+import { Combobox } from './shadcn-combobox';
+
+interface IndicatorSelectionProps {
+	selectedIndicatorId: string;
+	setSelectedIndicatorId: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export const IndicatorSelection = ({ selectedIndicatorId, setSelectedIndicatorId }: IndicatorSelectionProps) => {
+	const { data: indicators } = useIndicators();
+
 	return (
-    <Combob
+		<Combobox
+			options={indicators?.map((indicator) => ({ value: indicator.id.toString(), label: indicator.name })) || []}
+			selectedValue={selectedIndicatorId}
+			onValueChange={setSelectedIndicatorId}
+			fuseKeys={['label']}
+			placeholder='Select an indicator'
+		/>
 	);
 };
