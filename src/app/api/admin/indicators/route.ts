09db3823b41d2ece379af9df72a1a04e9fcf22f3
Anton Siligan <anton.siligan@neinja.dev>
2025-12-21 15:18:51 +0100

    fix col name

--- src/app/api/admin/indicators/route.ts
+++ src/app/api/admin/indicators/route.ts
@@ -104,7 +104,7 @@ export const POST = async () => {
 					const dbSource = await adminSupabase
 						.schema(DatabaseSchema.DATA)
 						.from(DatabaseTable.FREQUENCY_SOURCES)
-						.insert({ frequency_id: dbFrequency.data.id, name: source.source, code: source.code })
+						.insert({ frequency_id: dbFrequency.data.id, data_source: source.source, code: source.code })
 						.select()
 						.single();
 
