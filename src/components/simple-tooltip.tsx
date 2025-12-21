0880cd27b5fae848ffb16a3a876e956695a68b3d
Anton Siligan <anton.siligan@neinja.dev>
2025-12-21 15:56:51 +0100

    remove underline from tooltips

--- src/components/simple-tooltip.tsx
+++ src/components/simple-tooltip.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { Tooltip, TooltipContent, TooltipTrigger } from './shadcn/ui/tooltip';
-import { cn } from '@/utils/shadcn/utils';
 
 export const SimpleTooltip = ({
 	children,
@@ -19,11 +18,11 @@ export const SimpleTooltip = ({
 		<Tooltip open={open} onOpenChange={setOpen}>
 			<TooltipTrigger asChild={asChild}>
 				<span
-					className={cn(
-						'border-b border-gray-500',
-						cursorPointer ? 'cursor-pointer' : 'cursor-default',
-						open ? 'border-solid' : 'border-dotted'
-					)}
+				// className={cn(
+				// 	'border-b border-gray-500',
+				// 	cursorPointer ? 'cursor-pointer' : 'cursor-default',
+				// 	open ? 'border-solid' : 'border-dotted'
+				// )}
 				>
 					{children}
 				</span>
