eadfd0e462507a3bbbb6b8ceecb7263b737e42b6
Anton Siligan <anton.siligan@neinja.dev>
2025-12-29 12:09:15 +0100

    add country group hooks

---
+++ src/hooks/react-query/mutations/add-country-to-group.ts
@@ -0,0 +1,14 @@
+import { addCountryToGroup } from '@/supabase/api/country_groups';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+export const useAddCountryToGroup = () => {
+	const queryClient = useQueryClient();
+
+	return useMutation({
+		mutationFn: ({ countryCode, groupId }: { countryCode: string; groupId: string }) =>
+			addCountryToGroup(countryCode, groupId),
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ['country-groups'] });
+		}
+	});
+};
---
+++ src/hooks/react-query/mutations/remove-country-from-group.ts
@@ -0,0 +1,12 @@
+import { removeCountryFromGroup } from '@/supabase/api/country_groups';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+export const useRemoveCountryFromGroup = () => {
+	const queryClient = useQueryClient();
+
+	return useMutation({
+		mutationFn: ({ countryCode, groupId }: { countryCode: string; groupId: string }) =>
+			removeCountryFromGroup(countryCode, groupId),
+		onSuccess: () => queryClient.invalidateQueries({ queryKey: ['country-groups'] })
+	});
+};
