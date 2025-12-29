d1e24c62d3ee988004ed8af456e1ecd2f1ce01c5
Anton Siligan <anton.siligan@neinja.dev>
2025-12-29 12:10:00 +0100

    remove favourite countries hook

--- src/hooks/react-query/queries/use-favourite-countries.ts
+++
@@ -1,9 +0,0 @@
-import { getFavouriteCountries } from '@/supabase/api/country_groups';
-import { useQuery } from '@tanstack/react-query';
-
-export const useFavouriteCountries = () => {
-	return useQuery<string[]>({
-		queryKey: ['favourite-countries'],
-		queryFn: () => getFavouriteCountries()
-	});
-};
