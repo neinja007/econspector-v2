e74d1e612b6ad76db8fdea08f614eaa5d3e70252
Anton Siligan <anton.siligan@neinja.dev>
2025-12-29 11:54:28 +0100

    add country group functions

--- src/supabase/api/favourite-countries.ts
+++ src/supabase/api/favourite-countries.ts
@@ -5,28 +5,29 @@ export const getFavouriteCountries = async (): Promise<string[]> => {
 	const { data, error } = await supabase
 
 		.schema(DatabaseSchema.USERS)
-		.from(DatabaseTable.FAVOURITE_COUNTRIES)
-		.select('country_cca3');
+		.from(DatabaseTable.COUNTRY_GROUPS)
+		.select('countries(*)')
+		.eq('name', 'Favourites');
 
 	if (error) throw error;
-	return data.map((country) => country.country_cca3);
+	return data[0].countries.map((country) => country.cca3);
 };
 
-export const addFavouriteCountry = async (countryCode: string) => {
+export const addCountryToGroup = async (countryCode: string, groupId: string) => {
 	const { data, error } = await supabase
 		.schema(DatabaseSchema.USERS)
-		.from(DatabaseTable.FAVOURITE_COUNTRIES)
-		.insert({ country_cca3: countryCode });
-
+		.from(DatabaseTable.COUNTRY_GROUPS_COUNTRIES)
+		.upsert({ group_id: groupId, country_cca3: countryCode }, { onConflict: 'country_group_id, country_cca3' });
 	if (error) throw error;
 	return data;
 };
 
-export const removeFavouriteCountry = async (countryCode: string) => {
+export const removeCountryFromGroup = async (countryCode: string, groupId: string) => {
 	const { data, error } = await supabase
 		.schema(DatabaseSchema.USERS)
-		.from(DatabaseTable.FAVOURITE_COUNTRIES)
+		.from(DatabaseTable.COUNTRY_GROUPS_COUNTRIES)
 		.delete()
+		.eq('group_id', groupId)
 		.eq('country_cca3', countryCode);
 
 	if (error) throw error;
