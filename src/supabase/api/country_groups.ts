ca90008171efbba40f6180c96add3bac05b8558b
Anton Siligan <anton.siligan@neinja.dev>
2025-12-29 12:01:49 +0100

    change name and add country groups select

--- src/supabase/api/country_groups.ts
+++ src/supabase/api/country_groups.ts
@@ -13,6 +13,15 @@ export const getFavouriteCountries = async (): Promise<string[]> => {
 	return data[0].countries.map((country) => country.cca3);
 };
 
+export const getCountryGroups = async (): Promise<{ id: string; name: string }[]> => {
+	const { data, error } = await supabase
+		.schema(DatabaseSchema.USERS)
+		.from(DatabaseTable.COUNTRY_GROUPS)
+		.select('id, name, description, core');
+	if (error) throw error;
+	return data;
+};
+
 export const addCountryToGroup = async (countryCode: string, groupId: string) => {
 	const { data, error } = await supabase
 		.schema(DatabaseSchema.USERS)
