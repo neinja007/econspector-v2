b8d9cb70a1428ed8fc7bcfd20d7f88f4846ab1dd
Anton Siligan <anton.siligan@neinja.dev>
2025-12-29 12:09:23 +0100

    add join to group query

--- src/supabase/api/country_groups.ts
+++ src/supabase/api/country_groups.ts
@@ -1,23 +1,13 @@
 import { DatabaseSchema, DatabaseTable } from '@/types/supabase';
 import { supabase } from '../clients/client';
 
-export const getFavouriteCountries = async (): Promise<string[]> => {
-	const { data, error } = await supabase
-
-		.schema(DatabaseSchema.USERS)
-		.from(DatabaseTable.COUNTRY_GROUPS)
-		.select('countries(*)')
-		.eq('name', 'Favourites');
-
-	if (error) throw error;
-	return data[0].countries.map((country) => country.cca3);
-};
-
-export const getCountryGroups = async (): Promise<{ id: string; name: string }[]> => {
+export const getCountryGroups = async (): Promise<
+	{ id: string; name: string; description: string; core: boolean; countries: string[] }[]
+> => {
 	const { data, error } = await supabase
 		.schema(DatabaseSchema.USERS)
 		.from(DatabaseTable.COUNTRY_GROUPS)
-		.select('id, name, description, core');
+		.select('id, name, description, core, countries(*)');
 	if (error) throw error;
 	return data;
 };
