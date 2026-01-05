3ae87096fadac4011f2c0acc2d3034d8b13f36cf
Anton Siligan <anton.siligan@neinja.dev>
2026-01-05 14:36:51 +0100

    fix indicator admin route

--- src/app/api/admin/indicators/route.ts
+++ src/app/api/admin/indicators/route.ts
@@ -1,10 +1,10 @@
 import { adminSupabase } from '@/supabase/clients/admin';
-import { DatabaseSchema, DatabaseTable } from '@/types/supabase';
 import { NextResponse } from 'next/server';
 import { indicators } from '../../../../../private/data/indicators';
 
 export const POST = async () => {
-	const existingIndicators = await adminSupabase.schema(DatabaseSchema.DATA).from(DatabaseTable.INDICATORS).select('*');
+	const existingIndicators = await adminSupabase.schema('data').from('indicators').select('*');
+	const dataSources = await adminSupabase.schema('data').from('data_sources').select('*');
 	const skippedIndicators = [];
 	const createdIndicators = [];
 
@@ -18,8 +18,8 @@ export const POST = async () => {
 
 		if ('subindicators' in indicator) {
 			const dbIndicator = await adminSupabase
-				.schema(DatabaseSchema.DATA)
-				.from(DatabaseTable.INDICATORS)
+				.schema('data')
+				.from('indicators')
 				.insert({ name: indicator.name, code: indicator.abbreviation })
 				.select()
 				.single();
@@ -31,8 +31,8 @@ export const POST = async () => {
 
 			for (const subindicator of indicator.subindicators) {
 				const dbSubindicator = await adminSupabase
-					.schema(DatabaseSchema.DATA)
-					.from(DatabaseTable.INDICATORS)
+					.schema('data')
+					.from('indicators')
 					.insert({
 						name: subindicator.name,
 						code: subindicator.abbreviation,
@@ -50,8 +50,8 @@ export const POST = async () => {
 
 				for (const frequency of subindicator.frequencies) {
 					const dbFrequency = await adminSupabase
-						.schema(DatabaseSchema.DATA)
-						.from(DatabaseTable.INDICATOR_FREQUENCIES)
+						.schema('data')
+						.from('indicator_frequencies')
 						.insert({ frequency: frequency.frequency, indicator_id: dbSubindicator.data.id })
 						.select()
 						.single();
@@ -62,10 +62,21 @@ export const POST = async () => {
 					}
 
 					for (const source of frequency.sources) {
+						const dataSourceId = dataSources.data?.find((ds) => ds.name === source.source)?.id;
+						const dataSource = dataSources.data?.find((ds) => ds.name === source.source);
+						if (!dataSourceId || !dataSource) {
+							console.error('Failed to find data source:', source.source);
+							continue;
+						}
 						const dbSource = await adminSupabase
-							.schema(DatabaseSchema.DATA)
-							.from(DatabaseTable.FREQUENCY_SOURCES)
-							.insert({ frequency_id: dbFrequency.data.id, data_source: source.source, 'wb-code': source.code })
+							.schema('data')
+							.from('frequency_sources')
+							.insert({
+								frequency_id: dbFrequency.data.id,
+								data_source: dataSourceId,
+								'wb-code': source.code,
+								origin: 'SOURCE'
+							})
 							.select()
 							.single();
 
@@ -77,8 +88,8 @@ export const POST = async () => {
 			}
 		} else {
 			const dbIndicator = await adminSupabase
-				.schema(DatabaseSchema.DATA)
-				.from(DatabaseTable.INDICATORS)
+				.schema('data')
+				.from('indicators')
 				.insert({
 					name: indicator.name,
 					code: indicator.abbreviation,
@@ -95,8 +106,8 @@ export const POST = async () => {
 
 			for (const frequency of indicator.frequencies) {
 				const dbFrequency = await adminSupabase
-					.schema(DatabaseSchema.DATA)
-					.from(DatabaseTable.INDICATOR_FREQUENCIES)
+					.schema('data')
+					.from('indicator_frequencies')
 					.insert({ frequency: frequency.frequency, indicator_id: dbIndicator.data.id })
 					.select()
 					.single();
@@ -107,10 +118,21 @@ export const POST = async () => {
 				}
 
 				for (const source of frequency.sources) {
+					const dataSourceId = dataSources.data?.find((ds) => ds.name === source.source)?.id;
+					const dataSource = dataSources.data?.find((ds) => ds.name === source.source);
+					if (!dataSourceId || !dataSource) {
+						console.error('Failed to find data source:', source.source);
+						continue;
+					}
 					const dbSource = await adminSupabase
-						.schema(DatabaseSchema.DATA)
-						.from(DatabaseTable.FREQUENCY_SOURCES)
-						.insert({ frequency_id: dbFrequency.data.id, data_source: source.source, 'wb-code': source.code })
+						.schema('data')
+						.from('frequency_sources')
+						.insert({
+							frequency_id: dbFrequency.data.id,
+							data_source: dataSourceId,
+							'wb-code': source.code,
+							origin: 'SOURCE'
+						})
 						.select()
 						.single();
 
@@ -129,9 +151,9 @@ export const POST = async () => {
 };
 
 export const DELETE = async () => {
-	await adminSupabase.schema(DatabaseSchema.DATA).from(DatabaseTable.INDICATORS).delete().neq('id', 0);
-	await adminSupabase.schema(DatabaseSchema.DATA).from(DatabaseTable.INDICATOR_FREQUENCIES).delete().neq('id', 0);
-	await adminSupabase.schema(DatabaseSchema.DATA).from(DatabaseTable.FREQUENCY_SOURCES).delete().neq('id', 0);
+	await adminSupabase.schema('data').from('indicators').delete().neq('id', 0);
+	await adminSupabase.schema('data').from('indicator_frequencies').delete().neq('id', 0);
+	await adminSupabase.schema('data').from('frequency_sources').delete().neq('id', 0);
 
 	return NextResponse.json({ message: 'Indicators deleted from database' });
 };
