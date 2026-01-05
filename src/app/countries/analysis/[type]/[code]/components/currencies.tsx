308803f1d810b2370b1c5d21f4b72716ea642cfe
Anton Siligan <anton.siligan@neinja.dev>
2026-01-05 12:12:41 +0100

    fix currency

--- src/app/countries/analysis/[type]/[code]/components/currencies.tsx
+++ src/app/countries/analysis/[type]/[code]/components/currencies.tsx
@@ -1,18 +1,18 @@
 import { Button } from '@/components/shadcn/ui/button';
-import { Currency } from '@/types/currency';
+import { DbDataTables } from '@/types/db/alias';
 import Link from 'next/link';
 
-export const Currencies = ({ data }: { data: Currency[] }) => {
+export const Currencies = ({ data }: { data: DbDataTables<'currencies'>[] }) => {
 	return (
 		<div className='flex flex-col gap-1 items-baseline text-muted-foreground'>
 			<span className='text-sm'>{data.length > 1 ? 'Currencies' : 'Currency'}:</span>
 			<div className='flex gap-2'>
 				{data.map((currency) => (
-					<Button key={currency.currency_code} variant='outline' size='sm' asChild>
+					<Button key={currency.code} variant='outline' size='sm' asChild>
 						<Link
-							key={currency.currency_code}
+							key={currency.code}
 							className='flex items-center gap-1 transition-colors hover:text-blue-500'
-							href={`/currencies/analysis/${currency.currency_code}`}
+							href={`/currencies/analysis/${currency.code}`}
 						>
 							{currency.symbol_native} {currency.name}
 						</Link>
