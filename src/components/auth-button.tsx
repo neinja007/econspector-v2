489b4e19b3e60557e3beb6b019214e958cd69fe3
Anton Siligan <anton.siligan@neinja.dev>
2026-01-06 11:09:28 +0100

    fix sign up button functionality

--- src/components/auth-button.tsx
+++ src/components/auth-button.tsx
@@ -3,14 +3,20 @@
 import { useUser } from '@/hooks/react-query/queries/use-user';
 import { Button } from './shadcn/ui/button';
 import { LogInIcon } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 
 export const AuthButton = () => {
 	const { data: user } = useUser();
+	const router = useRouter();
+
+	const handleSignUp = () => {
+		router.push('/auth/sign-up');
+	};
 
 	return user ? null : (
-		<Button variant='outline'>
+		<Button variant='outline' onClick={handleSignUp}>
 			<LogInIcon className='size-4' />
-			<span>Sign in</span>
+			<span>Sign up</span>
 		</Button>
 	);
 };
